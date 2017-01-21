acef7aadcb6b999a501a0bc9516142b39e2c8894
Jake Goulding <jake.goulding@gmail.com>
2017-01-21 16:02:56 -0500

    dead code

--- examples/main.rs
+++ examples/main.rs
@@ -29,7 +29,6 @@ fn main() {
     let mut s = String::new();
     f.read_to_string(&mut s).expect("Can't read");
     let file = strata_rs::parse_rust_file(&s).expect("Failed");
-//    println!("{:#?}", file);
     let mut d = Done::default();
 
     for i in &file {
