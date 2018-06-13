d21c663de3d6b7f76e1c92c0639f8ca5909226d8
Jake Goulding <jake.goulding@gmail.com>
2018-06-13 17:45:49 -0400

    Add types to the body of the structured query reducer

--- ui/app/reducer/structuredQuery.ts
+++ ui/app/reducer/structuredQuery.ts
@@ -1,51 +1,95 @@
-import { Kind, makeNothing, FlatQueryItems } from 'app/types';
+import { Kind, makeNothing, FlatQueryItems, isBinaryKind, FlatBinaryItem, isFlatBinaryItem } from 'app/types';
 import { ActionType, Action } from 'app/actions';
 
 import { initial, forTarget } from './higherOrder';
 
-export function rawReducer(state, action: Action) {
+export type State = FlatQueryItems;
+
+export function rawReducer(state: State, action: Action): State {
     switch (action.type) {
         case ActionType.StructuredQueryKindUpdate: {
             const { kind, id } = action.payload;
-            const old = state[id];
 
-            if (old.lhs === undefined || old.rhs === undefined) {
-                const lhs = Object.keys(state).length;
-                const rhs = lhs + 1;
+            if (isBinaryKind(kind)) {
+                const old = state[id];
+
+                if (isFlatBinaryItem(old)) {
+                    const { lhs, rhs } = old;
+                    const newx: FlatBinaryItem = { kind, lhs, rhs } as FlatBinaryItem; // xxx
+
+                    return {
+                        ...state,
+                        [id]: newx,
+                    }
+                } else {
+                    // This isn't as efficient as it could be. Every time we
+                    // change to a binary type, we create brand new children
+                    // nodes and never recycle them.
+                    const lhs = Object.keys(state).length;
+                    const rhs = lhs + 1;
+                    const newx: FlatBinaryItem = { kind, lhs, rhs } as FlatBinaryItem; // xxx
+
+                    return {
+                        ...state,
+                        [id]: newx,
+                        [lhs]: makeNothing(),
+                        [rhs]: makeNothing(),
+                    }
+                }
+            } else if (kind === Kind.Layer) {
                 return {
                     ...state,
-                    [lhs]: makeNothing(),
-                    [rhs]: makeNothing(),
-                    [id]: { ...old, kind, lhs, rhs }
-                };
-            } else {
-                return { ...state, [id]: { ...old, kind } };
+                    [id]: { kind, name: '' }
+                }
+            } else if (kind === Kind.Nothing) {
+                return {
+                    ...state,
+                    [id]: { kind }
+                }
+            } else if (kind === Kind.Term) {
+                return {
+                    ...state,
+                    [id]: { kind, name: '', value: '' }
+                }
             }
+
+            const unreachable: never = kind;
+            return unreachable;
         }
         case ActionType.LayerNameUpdate: {
             const { name, id } = action.payload;
             const old = state[id];
 
-            return { ...state, [id]: { ...old, name } };
+            if (old.kind === Kind.Layer) {
+                return { ...state, [id]: { ...old, name } };
+            } else {
+                return state;
+            }
         }
         case ActionType.TermNameUpdate: {
             const { name, id } = action.payload;
             const old = state[id];
 
-            return { ...state, [id]: { ...old, name } };
+            if (old.kind === Kind.Term) {
+                return { ...state, [id]: { ...old, name } };
+            } else {
+                return state;
+            }
         }
         case ActionType.TermValueUpdate: {
             const { value, id } = action.payload;
             const old = state[id];
 
-            return { ...state, [id]: { ...old, value } };
+            if (old.kind === Kind.Term) {
+                return { ...state, [id]: { ...old, value } };
+            } else {
+                return state;
+            }
         }
         default:
             return state;
     }
 }
-
-export type State = FlatQueryItems;
 
 const initialStructuredQuery: State = {
     0: { kind: Kind.Containing, lhs: 1, rhs: 2 },
--- ui/app/types.ts
+++ ui/app/types.ts
@@ -21,10 +21,31 @@ export type BinaryKind =
     | Kind.OneOf
     ;
 
+export function isBinaryKind(kind: Kind): kind is BinaryKind {
+    switch (kind) {
+        case Kind.Containing:
+        case Kind.ContainedIn:
+        case Kind.NotContaining:
+        case Kind.NotContainedIn:
+        case Kind.OneOf:
+        case Kind.BothOf:
+        case Kind.FollowedBy:
+            return true;
+        default:
+            return false;
+    }
+}
+
 // The flat query is stored in the state and is a flat mapping of ID number to data
 
 export type FlatQueryItem =
     | FlatTerm
+    | FlatBinaryItem
+    | FlatLayer
+    | FlatNothing
+    ;
+
+export type FlatBinaryItem =
     | FlatBinary<Kind.Containing>
     | FlatBinary<Kind.ContainedIn>
     | FlatBinary<Kind.NotContaining>
@@ -32,14 +53,7 @@ export type FlatQueryItem =
     | FlatBinary<Kind.OneOf>
     | FlatBinary<Kind.BothOf>
     | FlatBinary<Kind.FollowedBy>
-    | FlatLayer
-    | FlatNothing;
-
-export interface FlatTerm {
-    kind: Kind.Term,
-    name: string,
-    value: string,
-}
+    ;
 
 export interface FlatBinary<T extends Kind> {
     kind: T,
@@ -47,16 +61,26 @@ export interface FlatBinary<T extends Kind> {
     rhs: number,
 }
 
+export function isFlatBinaryItem(item: FlatQueryItem): item is FlatBinaryItem {
+    return isBinaryKind(item.kind);
+}
+
+export interface FlatTerm {
+    kind: Kind.Term,
+    name: string,
+    value: string,
+}
+
 export interface FlatLayer {
     kind: Kind.Layer,
     name: string,
 }
 
-export const makeNothing: () => FlatNothing = () => ({ kind: Kind.Nothing });
-
 export interface FlatNothing {
     kind: Kind.Nothing,
 }
+
+export const makeNothing: () => FlatNothing = () => ({ kind: Kind.Nothing });
 
 export interface FlatQueryItems {
     [index: number]: FlatQueryItem;
